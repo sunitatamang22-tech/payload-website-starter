6ce8bef8b5eb66da3f2354e2c06a2061f2d2054b
sunitatamang22-tech <sunitapakhrin8494@gmail.com>
2025-06-04 20:46:38 +0930

    header

--- next.config.js
+++ next.config.js
@@ -1,18 +1,10 @@
-import { withPayload } from '@payloadcms/next/withPayload'
+const withPayload = require('payload/config')
 
-import redirects from './redirects.js'
-
-const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
-  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : undefined || process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3000'
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
+      ...[process.env.NEXT_PUBLIC_SERVER_URL].map((item) => {
         const url = new URL(item)
-
         return {
           hostname: url.hostname,
           protocol: url.protocol.replace(':', ''),
@@ -21,12 +13,7 @@ const nextConfig = {
     ],
   },
   reactStrictMode: true,
-  redirects,
-}
 
-export default withPayload(nextConfig, { devBundleServerPackages: false })
-
-module.exports = {
   async headers() {
     return [
       {
@@ -53,3 +40,7 @@ module.exports = {
     ]
   },
 }
+
+module.exports = withPayload(nextConfig, {
+  devBundleServerPackages: false,
+})
